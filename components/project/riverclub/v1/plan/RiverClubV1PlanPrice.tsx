e1fe9a8bce7202c1ddacc79c8edb1668f9f01abc
Enkhbayaar <145101106+Enkhbayaar@users.noreply.github.com>
2024-04-10 16:47:53 +0800

    cccc

--- components/project/riverclub/v1/plan/RiverClubV1PlanPrice.tsx
+++ components/project/riverclub/v1/plan/RiverClubV1PlanPrice.tsx
@@ -21,7 +21,7 @@ import { useEvent } from "react-use";
 const RiverClubV1PlanPrice = () => {
   const { readyDatasrc } = useContext(WidgetWrapperContext);
 
-  // Cookies.set("customer", { customerId: "17097020804073" });
+  Cookies.set("customer", { customerId: "17097020804073" });
 
   const { callProcess, isProcessWorking } = useCallProcess();
   const [selectDateModal, setSelectDateModal] = useState(false);
