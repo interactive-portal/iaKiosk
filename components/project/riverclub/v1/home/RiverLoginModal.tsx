de4e7da4416ade24e8514273ad0a8ed3604af29e
Enkhbayaar <145101106+Enkhbayaar@users.noreply.github.com>
2024-05-09 15:07:07 +0800

    ccc

--- components/project/riverclub/v1/home/RiverLoginModal.tsx
+++ components/project/riverclub/v1/home/RiverLoginModal.tsx
@@ -69,6 +69,9 @@ const RiverLoginModal: FC<PropsType> = ({
       // setNeedSignUp(true);
       // setSignIn(true);
       // setOpenModal(false);
+      if (!setNeedSignUp) {
+        setOpenModal(false);
+      }
       // setNeedSignUp(true);
       // }
     };
