de0f90b6a51e5139981e0882b501761041ae3751
Togtokhsuren <143073054+interactive-portal@users.noreply.github.com>
2024-08-21 15:31:27 +0800

    ss
    ss

--- pages/kiosk/kioskLayout.tsx
+++ pages/kiosk/kioskLayout.tsx
@@ -4,37 +4,45 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   return (
     <div
-      className="w-screen h-screen flex flex-col  justify-center items-center  "
+      className="w-screen max-w-[1080px] h-screen flex flex-col relative"
       style={{
         backgroundImage: "url(/images/home1.png)",
         backgroundRepeat: "repeat",
         backgroundSize: "cover",
       }}
     >
-      <div className="w-full py-6 md:py-10 flex items-center justify-center uppercase">
-        <img
-          src="/images/logo.png"
-          alt="home"
-          className="max-w-[100px] md:max-w-[400px]"
-          onClick={() => router.push("/kiosk/home")}
-        />
-      </div>
-      <div className="flex justify-center uppercase h-screen text-center lg:items-start w-full overflow-y-auto">
-        <div>{children}</div>
-      </div>
-      <div>
-        <button
-          className="absolute bottom-6 left-6 md:bottom-10 md:left-10 text-white uppercase text-[24px] md:text-[48px]"
-          onClick={() => router.push("/kiosk")}
-        >
-          home
-        </button>
-        <button
-          className="absolute bottom-6 right-6 md:bottom-10 md:right-10 text-white uppercase text-[24px] md:text-[48px]"
-          onClick={() => router.back()}
-        >
-          back
-        </button>
+      <div className="flex items-stretch justify-items-center text-center h-full  w-full">
+        <div className=" self-start  justify-items-center  w-full absolute top-2 ">
+          <img
+            src="/images/logo.png"
+            alt="home"
+            className="max-w-[300px] pt-6 md:max-w-[400px] mx-auto"
+            onClick={() => router.push("/kiosk/home")}
+          />
+        </div>
+        <div className="self-center justify-items-center mx-auto h-[78%] mt-10 overflow-hidden w-full ">
+          <div className="h-full items-center flex ">
+            <div className="mx-auto flex-none min-w-full px-4  overflow-hidden lg:overflow-auto scrollbar:!w-1.5 scrollbar:!h-1.5 scrollbar:bg-transparent scrollbar-track:!bg-slate-100 scrollbar-thumb:!rounded scrollbar-thumb:!bg-slate-300 scrollbar-track:!rounded dark:scrollbar-track:!bg-slate-500/[0.16] dark:scrollbar-thumb:!bg-slate-500/50 max-h-[98%] lg:supports-scrollbars:pr-2  kioskcontent">
+              {children}
+            </div>
+          </div>
+        </div>
+        <div className=" absolute bottom-4 self-end w-full">
+          <div className="w-full flex justify-between px-16">
+            <button
+              className=" flex text-white uppercase text-[24px] md:text-[48px] hover:text-opacity-75"
+              onClick={() => router.push("/kiosk")}
+            >
+              home
+            </button>
+            <button
+              className="  text-white uppercase text-[24px] md:text-[48px] hover:text-opacity-75"
+              onClick={() => router.back()}
+            >
+              back
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
